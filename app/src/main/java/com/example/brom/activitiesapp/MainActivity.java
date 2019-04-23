dff13833a01654f1a7262cbdea1db4957a1090eb
b18gussv <b18gussv@student.his.se>
2019-04-23 11:16:08 +0200

    Länkade ihop acitivties med onclicklistener och en ny metod.

--- app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -1,19 +1,50 @@
 package com.example.brom.activitiesapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity<val> extends AppCompatActivity {
+    private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali", "Kebnekaise","K2","Mount Everest","Kangchenjunga"};
     private String[] mountainLocations = {"Alps","Alps","Alaska"};
     private int[] mountainHeights ={4478,4808,6190};
     // Create ArrayLists from the raw data above and use these lists when populating your ListView.
+    private ArrayList<String> listData=new ArrayList<>(Arrays.asList(mountainNames));
+    private ArrayList<Mountain> mountainArrayList=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mountainArrayList.add(new Mountain("Matterhorn","Alperna",4478));
+        mountainArrayList.add(new Mountain("Mont Blanc","Alperna",4808));
+        mountainArrayList.add(new Mountain("Denali","Alaska",6190));
+        mountainArrayList.add(new Mountain("Kebnekaise","Skanderna",2106));
+        mountainArrayList.add(new Mountain("K2","Himalaya",8611));
+        mountainArrayList.add(new Mountain("Mount Everest","Himalaya",8848));
+        mountainArrayList.add(new Mountain("Kangchenjunga","Himalaya",8586));
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.list_items,R.id.list_item_textview,listData);
+        final ListView my_listview=(ListView) findViewById(R.id.list_item_textview);
+        my_listview.setAdapter(adapter);
 
+        my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                donk();
+            }
+        });
         // 1. Create a ListView as in previous assignment
         // 2. Create a new activity named "MountainDetailsActivity
         // 3. Create a new Layout file for the MountainDetailsActivity called
@@ -33,5 +64,10 @@ public class MainActivity extends AppCompatActivity {
         // 8. From the MountainDetailsActivity you should have an option to "go back" using an
         //    left arro button. This is done by letting the MainActivity be the parent activity to
         //    MountainDetailsActivity.
+    }
+
+    private void donk() {
+        Intent startNewActivity = new Intent(this, MountainDetailsActivity.class);
+        startActivity(startNewActivity);
     }
 }
